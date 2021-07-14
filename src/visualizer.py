078d53063afd68217ddc6b2288df03b6cb5aa6a9
Enhao Zhang <zhang.eh@outlook.com>
2021-07-14 23:07:25 +0000

    remove event_name argument from Visualizer's visualize_results()

--- src/visualizer.py
+++ src/visualizer.py
@@ -17,12 +17,12 @@ class Visualizer:
             for row in out_stream:
                 writer.writerow([row[0], row[1]])
                 
-    def visualize_results(self, out_stream, input_video_fn, event_name, train_or_val, pos_or_neg, write_csv=False):
+    def visualize_results(self, out_stream, input_video_fn, train_or_val, pos_or_neg, write_csv=False):
         if write_csv:
             self.write_csv(out_stream)
         video = cv2.VideoCapture(os.path.join(self.input_video_dir, input_video_fn))
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        fps = video.get(cv2.CAP_PROP_FPS)
+        # num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        # fps = video.get(cv2.CAP_PROP_FPS)
         
         directory = "{0}/{1}/{2}/".format(self.output_dir, train_or_val, pos_or_neg)
         if not os.path.exists(directory):
