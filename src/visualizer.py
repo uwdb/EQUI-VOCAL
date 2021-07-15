6fd4d181002f00ef3b73307850449f929957a79a
Enhao Zhang <zhang.eh@outlook.com>
2021-07-15 01:06:12 +0000

    add start_time and end_time fields to the out_stream

--- src/visualizer.py
+++ src/visualizer.py
@@ -11,25 +11,26 @@ class Visualizer:
         # Write start_time, end_time to csv file
         with open(self.out_csv_fn, 'w') as f:
             writer = csv.writer(f)
-            csv_line = "start_time, end_time"
+            csv_line = "frame_id, start_time, end_time"
             writer.writerows([csv_line.split(',')])
-            
+
             for row in out_stream:
-                writer.writerow([row[0], row[1]])
-                
+                writer.writerow(row)
+
     def visualize_results(self, out_stream, input_video_fn, train_or_val, pos_or_neg, write_csv=False):
         if write_csv:
             self.write_csv(out_stream)
         video = cv2.VideoCapture(os.path.join(self.input_video_dir, input_video_fn))
         # num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         # fps = video.get(cv2.CAP_PROP_FPS)
-        
+
         directory = "{0}/{1}/{2}/".format(self.output_dir, train_or_val, pos_or_neg)
         if not os.path.exists(directory):
             os.makedirs(directory)
-            
+
         # Visualize bboxes
-        for i, frame_id in enumerate(out_stream):
+        for i, row in enumerate(out_stream):
+            frame_id, _, _ = row
             video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
             ret, frame = video.read()
             cv2.imwrite("{0}/{1}_{2}.jpg".format(directory, input_video_fn[:-4], i), frame)
