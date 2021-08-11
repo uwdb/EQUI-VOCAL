061379ec115cd3b6a66f536e427e271989191254
Enhao Zhang <zhang.eh@outlook.com>
2021-08-11 21:27:18 +0000

    create base class for loading fasterRCNN model

---
+++ src/faster_r_cnn.py
@@ -0,0 +1,16 @@
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+
+
+# Base class for loading object detection
+class FasterRCNN:
+    def __init__(self):
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6  # set threshold for this model
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+        self.model = build_model(cfg) # returns a torch.nn.Module
+        DetectionCheckpointer(self.model).load('/home/ubuntu/complex_event_video/src/model_final_280758.pkl')
+        self.model.train(False) # inference mode
