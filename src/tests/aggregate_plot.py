3ee9c77611f31f88874ac9d144a709d8175c68c6
Enhao Zhang <zhang.eh@outlook.com>
2021-08-14 00:01:00 +0000

    add ann option where x axis of the plot is the number of negative frames observed

--- src/tests/aggregate_plot.py
+++ src/tests/aggregate_plot.py
@@ -5,19 +5,19 @@ import json
 if __name__ == '__main__':
     fig, ax = plt.subplots(1)
 
-    with open('iterative_processing.json', 'r') as f:
+    with open('iterative.json', 'r') as f:
         iterative_y_list = json.loads(f.read())
     for plot_data_y in iterative_y_list:
         x_values = range(len(plot_data_y))
         ax.plot(x_values, plot_data_y, color='tab:blue', alpha=0.3)
 
-    with open('random_processing.json', 'r') as f:
+    with open('random.json', 'r') as f:
         random_y_list = json.loads(f.read())
     for plot_data_y in random_y_list:
         x_values = range(len(plot_data_y))
         ax.plot(x_values, plot_data_y, color='tab:orange', alpha=0.3)
 
-    with open('random_processing_with_roi.json', 'r') as f:
+    with open('random_roi.json', 'r') as f:
         random_roi_y_list = json.loads(f.read())
     for plot_data_y in random_roi_y_list:
         x_values = range(len(plot_data_y))
@@ -25,5 +25,7 @@ if __name__ == '__main__':
 
     ax.set_ylabel('number of positive instances the user finds')
     ax.set_xlabel('number of frames that user has seen')
+    # ax.set_xlabel('number of negative frames that user has seen')
+    # ax.set_xscale('log')
     ax.grid()
-    plt.savefig("aggregate_plot")
+    plt.savefig("aggregate")
