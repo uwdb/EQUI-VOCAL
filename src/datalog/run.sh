c23a27e0f498f9ce680113e22d1716ba3e464115
Enhao Zhang <zhang.eh@outlook.com>
2021-11-11 20:11:27 +0000

    benchmarking some queries

--- src/datalog/run.sh
+++ src/datalog/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-time /home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffle q2.2.dl -p data/profile_log_files/q2.2_inline.log -j 1
+time /home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffle queries/q1.2_filter-seq.dl -p data/profile_log_files/q1.2_filter-seq.log -j 1 
+
+# time /home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffle queries/q2.2.dl -p data/profile_log_files/q2.2.log -j 1 
