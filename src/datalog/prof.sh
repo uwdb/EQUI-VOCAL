6a4b2036bbd899d9b587d41d45573d739683cab8
Enhao Zhang <zhang.eh@outlook.com>
2021-11-11 20:11:27 +0000

    benchmarking some queries

--- src/datalog/prof.sh
+++ src/datalog/prof.sh
@@ -1,1 +1,3 @@
-/home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffleprof data/profile_log_files/q2.2_inline.log
+/home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffleprof data/profile_log_files/q1.2_filter-seq.log
+
+# /home/ubuntu/complex_event_video/src/datalog/souffle/install/bin/souffleprof data/profile_log_files/q2.2.log
