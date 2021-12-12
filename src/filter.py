dd4bcace41307ebc6b178aa2ac869f283fd95d87
Enhao Zhang <zhang.eh@outlook.com>
2021-12-12 10:10:16 +0000

    restructure

--- src/filter.py
+++ src/filter.py
@@ -5,39 +5,90 @@ from glob import glob
 import yaml
 import numpy as np
 
+def construct_spatial_feature(bbox):
+    x1, y1, x2, y2 = bbox
+    centroid_x = (x1 + x2) / 2
+    centroid_y = (y1 + y2) / 2
+    width = x2 - x1
+    height = y2 - y1
+    wh_ratio = width / height
+    return np.array([centroid_x, centroid_y, width, height, wh_ratio])
 
-def car_and_pedestrain_at_intersection(res_per_frame, frame_id):
+def construct_spatial_feature_two_objects(bbox1, bbox2):
+    x11, y11, x21, y21 = bbox1
+    centroid_x1 = (x11 + x21) / 2
+    centroid_y1 = (y11 + y21) / 2
+    width1 = x21 - x11
+    height1 = y21 - y11
+    wh_ratio1 = width1 / height1
+
+    x12, y12, x22, y22 = bbox2
+    centroid_x2 = (x12 + x22) / 2
+    centroid_y2 = (y12 + y22) / 2
+    width2 = x22 - x12
+    height2 = y22 - y12
+    wh_ratio2 = width2 / height2
+    return np.array([centroid_x1, centroid_y1, width1, height1, wh_ratio1, centroid_x2, centroid_y2, width2, height2, wh_ratio2])
+
+def construct_spatial_feature_spatial_relationship(car_box, person_box):
+    x, y, x2, y2 = car_box
+    xp, yp, x4, y4 = person_box
+    w = x2 - x
+    h = y2 - y
+    wp = x4 - xp
+    hp = y4 - yp
+    s1 = (x - xp) / w
+    s2 = (y - yp) / h
+    s3 = (y + h - yp - hp) / h
+    s4 = (x + w - xp - wp) / w
+    s5 = hp / h
+    s6 = wp / w
+    s7 = (wp * hp) / (w * h)
+    s8 = (wp + hp) / (w + h)
+    return np.array([s1, s2, s3, s4, s5, s6, s7, s8])
+
+def turning_car_and_pedestrain_at_intersection(maskrcnn_bboxes):
+    n_frames =len(maskrcnn_bboxes)
+    spatial_feature_dim = 5
+    feature_names = ["x", "y", "w", "h", "r"]
     edge_corner_bbox = (367, 345, 540, 418)
-    has_car = 0
-    has_pedestrian = 0
-    for x1, y1, x2, y2, class_name, score in res_per_frame:
-        if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
-            has_pedestrian = 1
-        elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
-            # Watch the video and identify the correct cars. Hardcode the correct car bbox to use.
-            if frame_id >= 14043 and frame_id <= 14079 and (x1 < 500 or x1 > 800):
-                continue
-            if frame_id >= 15312 and frame_id <= 15365 and y2 < 450:
-                continue
-            if frame_id >= 15649 and frame_id <= 15722 and x1 < 200:
-                continue
-            if frame_id >= 16005 and frame_id <= 16044 and y2 < 430:
-                continue
-            if frame_id >= 16045 and frame_id <= 16072 and x1 < 250:
-                continue
-            if frame_id >= 16073 and frame_id <= 16090 and y2 < 450:
-                continue
-            if frame_id >= 16091 and frame_id <= 16122 and x1 < 245:
-                continue
-            if frame_id >= 16123 and frame_id <= 16153 and x1 > 500:
-                continue
-            if frame_id >= 22375 and frame_id <= 22430 and y2 < 500:
-                continue
-            has_car = 1
-            car_x1, car_y1, car_x2, car_y2 = x1, y1, x2, y2
+    spatial_features = np.zeros((n_frames, spatial_feature_dim), dtype=np.float64)
+    # Filtering stage
+    candidates = np.full(n_frames, True, dtype=np.bool)
+    for frame_id in range(n_frames):
+        res_per_frame = maskrcnn_bboxes["frame_{}.jpg".format(frame_id)]
+        has_car = 0
+        has_pedestrian = 0
+        for x1, y1, x2, y2, class_name, _ in res_per_frame:
+            if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
+                has_pedestrian = 1
+            elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
+                # Watch the video and identify the correct cars. Hardcode the correct car bbox to use.
+                if frame_id >= 14043 and frame_id <= 14079 and (x1 < 500 or x1 > 800):
+                    continue
+                if frame_id >= 15312 and frame_id <= 15365 and y2 < 450:
+                    continue
+                if frame_id >= 15649 and frame_id <= 15722 and x1 < 200:
+                    continue
+                if frame_id >= 16005 and frame_id <= 16044 and y2 < 430:
+                    continue
+                if frame_id >= 16045 and frame_id <= 16072 and x1 < 250:
+                    continue
+                if frame_id >= 16073 and frame_id <= 16090 and y2 < 450:
+                    continue
+                if frame_id >= 16091 and frame_id <= 16122 and x1 < 245:
+                    continue
+                if frame_id >= 16123 and frame_id <= 16153 and x1 > 500:
+                    continue
+                if frame_id >= 22375 and frame_id <= 22430 and y2 < 500:
+                    continue
+                has_car = 1
+                car_x1, car_y1, car_x2, car_y2 = x1, y1, x2, y2
         if has_car and has_pedestrian:
-            return True, (car_x1, car_y1, car_x2, car_y2)
-    return False, None
+            spatial_features[frame_id] = construct_spatial_feature((car_x1, car_y1, car_x2, car_y2))
+        else:
+            candidates[frame_id] = False
+    return feature_names, spatial_feature_dim, spatial_features, candidates
 
 def meva_person_stands_up(maskrcnn_bboxes, video_list, pos_frames):
     n_frames = len(maskrcnn_bboxes)
@@ -85,20 +136,11 @@ def meva_person_stands_up(maskrcnn_bboxes, video_list, pos_frames):
                         break
     return feature_names, spatial_feature_dim, spatial_features, candidates
 
-def construct_spatial_feature(bbox):
-    x1, y1, x2, y2 = bbox
-    centroid_x = (x1 + x2) / 2
-    centroid_y = (y1 + y2) / 2
-    width = x2 - x1
-    height = y2 - y1
-    wh_ratio = width / height
-    return np.array([centroid_x, centroid_y, width, height, wh_ratio])
-
-def test_a(res_per_frame, frame_id):
+def test_a(maskrcnn_bboxes):
     predicate = lambda x1, y1, x2, y2 : 1.0 * (x2 - x1) / (y2 - y1) > 2
-    return template_one_object(res_per_frame, frame_id, predicate)
+    return template_one_object(maskrcnn_bboxes, predicate)
 
-def test_b(res_per_frame, frame_id):
+def test_b(maskrcnn_bboxes):
     def predicate(x1, y1, x2, y2):
         x = (x1 + x2) / 2
         y = (y1 + y2) / 2
@@ -106,9 +148,9 @@ def test_b(res_per_frame, frame_id):
         h = y2 - y1
         r = w / h
         return (r < 2 and x > 250)
-    return template_one_object(res_per_frame, frame_id, predicate)
+    return template_one_object(maskrcnn_bboxes, predicate)
 
-def test_c(res_per_frame, frame_id):
+def test_c(maskrcnn_bboxes):
     def predicate(x1, y1, x2, y2):
         x = (x1 + x2) / 2
         y = (y1 + y2) / 2
@@ -116,31 +158,39 @@ def test_c(res_per_frame, frame_id):
         h = y2 - y1
         r = w / h
         return (r < 2 and x > 250) or (w > 100 and h > 50)
-    return template_one_object(res_per_frame, frame_id, predicate)
+    return template_one_object(maskrcnn_bboxes, predicate)
 
-
-def template_one_object(res_per_frame, frame_id, predicate):
+def template_one_object(maskrcnn_bboxes, predicate):
+    n_frames =len(maskrcnn_bboxes)
+    spatial_feature_dim = 5
+    feature_names = ["x", "y", "w", "h", "r"]
     edge_corner_bbox = (367, 345, 540, 418)
-    has_car_and_satisfies_predicate = 0
-    has_car = 0
-    has_pedestrian = 0
-    for x1, y1, x2, y2, class_name, score in res_per_frame:
-        if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
-            has_pedestrian = 1
-        elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
-            car_x1, car_y1, car_x2, car_y2 = x1, y1, x2, y2
-            has_car = 1
-            if predicate(x1, y1, x2, y2):
-                has_car_and_satisfies_predicate = 1
-                acar_x1, acar_y1, acar_x2, acar_y2 = x1, y1, x2, y2
-        if has_car_and_satisfies_predicate and has_pedestrian:
-            return True, (acar_x1, acar_y1, acar_x2, acar_y2)
-    if has_car and has_pedestrian:
-        return True, (car_x1, car_y1, car_x2, car_y2)
-    return False, None
+    spatial_features = np.zeros((n_frames, spatial_feature_dim), dtype=np.float64)
+    # Filtering stage
+    candidates = np.full(n_frames, True, dtype=np.bool)
+    for frame_id in range(n_frames):
+        res_per_frame = maskrcnn_bboxes["frame_{}.jpg".format(frame_id)]
+        car_boxes = []
+        person_boxes = []
+        car_satisfies_predicate_boxes = []
+        for x1, y1, x2, y2, class_name, _ in res_per_frame:
+            if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
+                person_boxes.append((x1, y1, x2, y2))
+            elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
+                car_boxes.append((x1, y1, x2, y2))
+                if predicate(x1, y1, x2, y2):
+                    car_satisfies_predicate_boxes.append((x1, y1, x2, y2))
+        if car_boxes and person_boxes:
+            if car_satisfies_predicate_boxes:
+                spatial_features[frame_id] = construct_spatial_feature(random.choice(car_satisfies_predicate_boxes))
+            else:
+                spatial_features[frame_id] = construct_spatial_feature(random.choice(car_boxes))
+        else:
+            candidates[frame_id] = False
+    return feature_names, spatial_feature_dim, spatial_features, candidates
 
 
-def test_d(res_per_frame):
+def test_d(maskrcnn_bboxes):
     def predicate1(x1, y1, x2, y2):
         x = (x1 + x2) / 2
         y = (y1 + y2) / 2
@@ -155,9 +205,9 @@ def test_d(res_per_frame):
         h = y2 - y1
         r = w / h
         return (x < 520 and r > 0.3)
-    return template_two_objects_b(res_per_frame, [predicate1, predicate2])
+    return template_two_objects_b(maskrcnn_bboxes, [predicate1, predicate2])
 
-def test_e(res_per_frame):
+def test_e(maskrcnn_bboxes):
     """
     Event definition:
     - Car and pedestrain at the intersection
@@ -179,7 +229,7 @@ def test_e(res_per_frame):
         s7 = (wp * hp) / (w * h)
         s8 = (wp + hp) / (w + h)
         return (s1 > 0.2 and s1 < 0.9 and s2 > 0.2 and s2 < 0.9)
-    return template_spatial_relationship(res_per_frame, predicate)
+    return template_spatial_relationship(maskrcnn_bboxes, predicate)
 
 def template_two_objects(res_per_frame, predicates):
     has_car_1 = 0
@@ -202,46 +252,68 @@ def template_two_objects(res_per_frame, predicates):
     return False, None, None
 
 
-def template_two_objects_b(res_per_frame, predicates):
+def template_two_objects_b(maskrcnn_bboxes, predicates):
+    n_frames =len(maskrcnn_bboxes)
+    spatial_feature_dim = 10
+    feature_names = ["x1", "y1", "w1", "h1", "r1", "x2", "y2", "w2", "h2", "r2"]
     edge_corner_bbox = (367, 345, 540, 418)
-    has_car_and_satisfies_predicate = 0
-    has_car = 0
-    has_pedestrian_and_satisfies_predicate = 0
-    has_pedestrian = 0
-    for x1, y1, x2, y2, class_name, _ in res_per_frame:
-        if class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2)):
-            person_box_candidate = (x1, y1, x2, y2)
-            has_pedestrian = 1
-            if predicates[1](x1, y1, x2, y2):
-                person_box_pos = (x1, y1, x2, y2)
-                has_pedestrian_and_satisfies_predicate = 1
-        elif class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2)):
-            car_box_candidate = (x1, y1, x2, y2)
-            has_car = 1
-            if predicates[0](x1, y1, x2, y2):
-                car_box_pos = (x1, y1, x2, y2)
-                has_car_and_satisfies_predicate = 1
-        if has_car_and_satisfies_predicate and has_pedestrian_and_satisfies_predicate:
-            return True, car_box_pos, person_box_pos
-    if has_car and has_pedestrian:
-        return True, car_box_candidate, person_box_candidate
-    return False, None, None
+    spatial_features = np.zeros((n_frames, spatial_feature_dim), dtype=np.float64)
+    # Filtering stage
+    candidates = np.full(n_frames, True, dtype=np.bool)
+    for frame_id in range(n_frames):
+        res_per_frame = maskrcnn_bboxes["frame_{}.jpg".format(frame_id)]
+        car_boxes = []
+        person_boxes = []
+        car_satisfies_predicate_boxes = []
+        person_satisfies_predicate_boxes = []
+        for x1, y1, x2, y2, class_name, _ in res_per_frame:
+            if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
+                person_boxes.append((x1, y1, x2, y2))
+                if predicates[1](x1, y1, x2, y2):
+                    person_satisfies_predicate_boxes.append((x1, y1, x2, y2))
+            elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
+                car_boxes.append((x1, y1, x2, y2))
+                if predicates[0](x1, y1, x2, y2):
+                    car_satisfies_predicate_boxes.append((x1, y1, x2, y2))
+        if car_satisfies_predicate_boxes and person_satisfies_predicate_boxes:
+            spatial_features[frame_id] = construct_spatial_feature_two_objects(random.choice(car_satisfies_predicate_boxes), random.choice(person_satisfies_predicate_boxes))
+        else:
+            if car_boxes and person_boxes:
+                spatial_features[frame_id] = construct_spatial_feature_two_objects(random.choice(car_boxes), random.choice(person_boxes))
+            else:
+                candidates[frame_id] = False
+    return feature_names, spatial_feature_dim, spatial_features, candidates
 
 
-def template_spatial_relationship(res_per_frame, predicate):
+def template_spatial_relationship(maskrcnn_bboxes, predicate):
+    n_frames =len(maskrcnn_bboxes)
+    spatial_feature_dim = 8
+    feature_names = ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"]
     edge_corner_bbox = (367, 345, 540, 418)
-    car_boxes = []
-    person_boxes = []
-    for x1, y1, x2, y2, class_name, _ in res_per_frame:
-        if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
-            person_boxes.append((x1, y1, x2, y2))
-        elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
-            car_boxes.append((x1, y1, x2, y2))
-    # Check whether contains the target spatial relationship
-    for car_box in car_boxes:
-        for person_box in person_boxes:
-            if predicate(car_box, person_box):
-                return True, car_box, person_box
-    if car_boxes and person_boxes:
-        return True, random.choice(car_boxes), random.choice(person_boxes)
-    return False, None, None
+    spatial_features = np.zeros((n_frames, spatial_feature_dim), dtype=np.float64)
+    # Filtering stage
+    candidates = np.full(n_frames, True, dtype=np.bool)
+    for frame_id in range(n_frames):
+        res_per_frame = maskrcnn_bboxes["frame_{}.jpg".format(frame_id)]
+        car_boxes = []
+        person_boxes = []
+        relationship_boxes = []
+        for x1, y1, x2, y2, class_name, _ in res_per_frame:
+            if (class_name == "person" and isOverlapping(edge_corner_bbox, (x1, y1, x2, y2))):
+                person_boxes.append((x1, y1, x2, y2))
+            elif (class_name in ["car", "truck"] and isInsideIntersection((x1, y1, x2, y2))):
+                car_boxes.append((x1, y1, x2, y2))
+        if car_boxes and person_boxes:
+            # Check whether contains the target spatial relationship
+            for car_box in car_boxes:
+                for person_box in person_boxes:
+                    if predicate(car_box, person_box):
+                        relationship_boxes.append((car_box, person_box))
+            if relationship_boxes:
+                car_box, person_box = random.choice(relationship_boxes)
+                spatial_features[frame_id] = construct_spatial_feature_spatial_relationship(car_box, person_box)
+            else:
+                spatial_features[frame_id] = construct_spatial_feature_spatial_relationship(random.choice(car_boxes), random.choice(person_boxes))
+        else:
+            candidates[frame_id] = False
+    return feature_names, spatial_feature_dim, spatial_features, candidates
