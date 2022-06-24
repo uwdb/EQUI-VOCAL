a4e15eafcb988168481eced9f06275242929e7ba
Enhao Zhang <enhaoz@cs.washington.edu>
2022-06-24 15:20:33 -0700

    add str_to_program() which converts string of program to executable program

--- src/quivr/logging.py
+++ src/quivr/logging.py
@@ -1,7 +1,8 @@
 import logging
 import os
 import dsl
-
+from tensorboard import program
+from pyparsing import nestedExpr
 
 def init_logging(save_path):
     logfile = os.path.join(save_path, 'log.txt')
@@ -18,11 +19,14 @@ def log_and_print(line):
     print(line)
     logging.info(line)
 
-def print_program(program, ignore_constants=True):
+def print_program(program):
     # if isinstance(program, dsl.Predicate) or isinstance(program, dsl.Hole):
     if issubclass(type(program), dsl.Predicate):
         if program.has_theta:
-            return program.name + "_" + str(abs(program.theta))
+            if program.with_hole:
+                return program.name + "_withHole"
+            else:
+                return program.name + "_" + str(abs(program.theta))
         else:
             return program.name
     if issubclass(type(program), dsl.Hole):
@@ -30,13 +34,59 @@ def print_program(program, ignore_constants=True):
     else:
         collected_names = []
         for submodule, functionclass in program.submodules.items():
-            collected_names.append(print_program(functionclass, ignore_constants=ignore_constants))
-        if program.has_params:
-            parameters = "params: {}".format(program.parameters.values())
-            if not ignore_constants:
-                collected_names.append(parameters)
+            collected_names.append(print_program(functionclass))
         joined_names = ', '.join(collected_names)
         return program.name + "(" + joined_names + ")"
+
+# convert a str back to an executable program; this is an inverse of print_program()
+def str_to_program(program_str):
+    if program_str.startswith("PredicateHole"):
+        return dsl.PredicateHole()
+    elif program_str.startswith("ParameterHole"):
+        functionclass = program_str.split("*")[1]
+        predicate = getattr(dsl, functionclass)
+        return dsl.ParameterHole(predicate())
+    elif program_str.startswith("Near"):
+        return dsl.Near(theta=-float(program_str.split("_")[1]))
+    elif program_str.startswith("Far"):
+        return dsl.Far(theta=float(program_str.split("_")[1]))
+    elif program_str.startswith("MinLength"):
+        return dsl.MinLength(theta=float(program_str.split("_")[1]))
+    elif program_str.startswith("True*"):
+        return dsl.TrueStar()
+    else:
+        idx = program_str.find("(")
+        idx_r = program_str.rfind(")")
+        # True*, Sequencing(Near_1.0, MinLength_10.0)
+        functionclass = program_str[:idx]
+        submodules = program_str[idx+1:idx_r]
+        counter = 0
+        submodule_list = []
+        submodule_start = 0
+        for i, char in enumerate(submodules):
+            if char == "," and counter == 0:
+                submodule_list.append(submodules[submodule_start:i])
+                submodule_start = i+2
+            elif char == "(":
+                counter += 1
+            elif char == ")":
+                counter -= 1
+        submodule_list.append(submodules[submodule_start:])
+        if functionclass == "Start":
+            program_init = getattr(dsl, "StartOperator")
+        if functionclass == "Sequencing":
+            program_init = getattr(dsl, "SequencingOperator")
+        elif functionclass == "Conjunction":
+            program_init = getattr(dsl, "ConjunctionOperator")
+        elif functionclass == "Kleene":
+            program_init = getattr(dsl, "KleeneOperator")
+        submodule_list = [str_to_program(submodule) for submodule in submodule_list]
+        return program_init(*submodule_list)
+
+
+
+
+
 
 def print_program_dict(prog_dict):
     log_and_print(print_program(prog_dict["program"], ignore_constants=True))
