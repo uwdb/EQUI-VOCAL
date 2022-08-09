cffd50388ef1197871c639c7c83d69031386eb6d
Enhao Zhang <enhaoz@cs.washington.edu>
2022-08-08 23:34:40 -0700

    add new script

---
+++ src/quivr/scripts/run_vocal.sh
@@ -0,0 +1,5 @@
+# python synthesize.py --method vocal --npred 5 --depth 3 --k 32 --max_duration 2
+
+# python synthesize.py --method vocal --npred 5 --depth 3 --k 32 --max_duration 2 --output_to_file
+
+python synthesize.py --method vocal --npred 5 --depth 3 --k 32 --max_duration 2 --query_str "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Front), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)" --output_to_file
