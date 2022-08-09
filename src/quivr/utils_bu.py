0e0e9e780da16ef6056ea77b4b0613d4cc4f13e7
Enhao Zhang <enhaoz@cs.washington.edu>
2022-08-08 23:32:01 -0700

    add construct_train_test(); support printing more predicates

--- src/quivr/utils_bu.py
+++ src/quivr/utils_bu.py
@@ -1,8 +1,14 @@
+import json
 import logging
 import os
+import random
+
+import numpy as np
+from sklearn.model_selection import train_test_split
 import dsl_bu as dsl
 
-def print_program(program):
+
+def print_program(program, as_dict_key=False):
     # if isinstance(program, dsl.Predicate) or isinstance(program, dsl.Hole):
     if issubclass(type(program), dsl.Predicate):
         if program.has_theta:
@@ -15,7 +21,10 @@ def print_program(program):
     if issubclass(type(program), dsl.Hole):
         return program.name
     if issubclass(type(program), dsl.DurationOperator):
-        return program.name + "(" + print_program(program.submodules["duration"]) + ", " + str(program.theta) + ")"
+        if as_dict_key:
+            return program.name + "(" + print_program(program.submodules["duration"]) + ")"
+        else:
+            return program.name + "(" + print_program(program.submodules["duration"]) + ", " + str(program.theta) + ")"
     else:
         collected_names = []
         for submodule, functionclass in program.submodules.items():
@@ -33,6 +42,22 @@ def str_to_program(program_str):
         return dsl.MinLength(theta=float(program_str.split("_")[1]))
     elif program_str.startswith("True*"):
         return dsl.TrueStar()
+    elif program_str.startswith("Right"):
+        return dsl.Right()
+    elif program_str.startswith("Left"):
+        return dsl.Left()
+    elif program_str.startswith("Front"):
+        return dsl.Front()
+    elif program_str.startswith("Back"):
+        return dsl.Back()
+    elif program_str.startswith("TopQuadrant"):
+        return dsl.TopQuadrant()
+    elif program_str.startswith("BottomQuadrant"):
+        return dsl.BottomQuadrant()
+    elif program_str.startswith("LeftQuadrant"):
+        return dsl.LeftQuadrant()
+    elif program_str.startswith("RightQuadrant"):
+        return dsl.RightQuadrant()
     else:
         idx = program_str.find("(")
         idx_r = program_str.rfind(")")
@@ -62,3 +87,60 @@ def str_to_program(program_str):
             return program_init(str_to_program(submodule_list[0]), int(submodule_list[1]))
         submodule_list = [str_to_program(submodule) for submodule in submodule_list]
         return program_init(*submodule_list)
+
+def construct_train_test(inputs_filename, labels_filename, n_labeled_pos=None, n_labeled_neg=None, n_train=None):
+    # read from json file
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}.json".format(inputs_filename), 'r') as f:
+        inputs = json.load(f)
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}.json".format(labels_filename), 'r') as f:
+        labels = json.load(f)
+
+    inputs = np.asarray(inputs, dtype=object)
+    labels = np.asarray(labels, dtype=object)
+
+    if n_train:
+        inputs_train, inputs_test, labels_train, labels_test = train_test_split(inputs, labels, train_size=n_train, random_state=42, stratify=labels)
+    if n_labeled_pos and n_labeled_neg:
+        n_pos = sum(labels)
+        sampled_labeled_index = random.sample(list(range(n_pos)), n_labeled_pos) + random.sample(list(range(n_pos, len(labels))), n_labeled_neg)
+        sampled_labeled_index = np.asarray(sampled_labeled_index)
+        print("before sort", sampled_labeled_index)
+        # sort sampled_labeled_index in ascending order
+        sampled_labeled_index = sampled_labeled_index[np.argsort(sampled_labeled_index)]
+        print("after sort", sampled_labeled_index)
+        inputs_train = inputs[sampled_labeled_index]
+        labels_train = labels[sampled_labeled_index]
+
+        remaining_index = np.delete(np.arange(len(labels)), sampled_labeled_index)
+
+        inputs_test = inputs[remaining_index]
+        labels_test = labels[remaining_index]
+        print("labels_test", labels_test)
+
+        n_pos = sum(labels_test)
+        n_neg = len(labels_test) - n_pos
+        print("n_pos", n_pos, "n_neg", n_neg)
+        if n_pos * 5 < n_neg:
+            inputs_test = inputs_test[:n_pos * 6]
+            labels_test = labels_test[:n_pos * 6]
+        else:
+            inputs_test = inputs_test[:int(n_neg/5)] + inputs_test[-int(n_neg/5)*5:]
+            labels_test = labels_test[:int(n_neg/5)] + labels_test[-int(n_neg/5)*5:]
+
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}_train.json".format(inputs_filename), 'w') as f:
+        json.dump(inputs_train.tolist(), f)
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}_train.json".format(labels_filename), 'w') as f:
+        json.dump(labels_train.tolist(), f)
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}_test.json".format(inputs_filename), 'w') as f:
+        json.dump(inputs_test.tolist(), f)
+    with open("/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/inputs/{}_test.json".format(labels_filename), 'w') as f:
+        json.dump(labels_test.tolist(), f)
+
+    print("inputs_train", len(inputs_train))
+    print("labels_train", len(labels_train), sum(labels_train))
+    print("inputs_test", len(inputs_test))
+    print("labels_test", len(labels_test), sum(labels_test))
+
+
+if __name__ == '__main__':
+    construct_train_test("Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Conjunction(Right, Front)), True*), Duration(Far_0.9, 2)), True*), Conjunction(Back, Left)), True*)_inputs", "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Conjunction(Right, Front)), True*), Duration(Far_0.9, 2)), True*), Conjunction(Back, Left)), True*)_labels", n_train=300)
