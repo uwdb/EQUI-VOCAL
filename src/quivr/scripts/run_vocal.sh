9d75f08fc1016e2e3a0c81dd9a5fbcdb46e69075
Enhao Zhang <enhaoz@cs.washington.edu>
2022-08-10 01:40:31 -0700

    update

--- src/quivr/scripts/run_vocal.sh
+++ src/quivr/scripts/run_vocal.sh
@@ -5,7 +5,7 @@
 # python synthesize.py --method vocal --npred 5 --depth 3 --k 32 --max_duration 2 --output_to_file
 
 # Exhaustive search. Query: collision
-python synthesize.py --method vocal_exhaustive --n_labeled_pos 1 --n_labeled_neg 5 --npred 5 --depth 3 --k 32 --max_duration 2 --output_to_file
+python synthesize.py --method vocal_exhaustive --n_labeled_pos 1 --n_labeled_neg 5 --npred 5 --depth 3 --k 32 --max_duration 2 --multithread 4 --output_to_file
 
 # Query 1: "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Front), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)"
 # Query 2: "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Conjunction(Front, Left)), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)"
