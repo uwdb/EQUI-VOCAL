e534bfb680e9390c95cd0e485ef9d81ac4437e9d
Enhao Zhang <enhaoz@cs.washington.edu>
2022-08-31 14:37:25 -0700

    update expansion rule such that a query can be constrcuted along different paths, as well as collapsing equivalence

--- src/quivr/query_graph.py
+++ src/quivr/query_graph.py
@@ -1,6 +1,6 @@
 import copy
 import quivr.dsl as dsl
-from quivr.utils import print_program
+from quivr.utils import print_program, rewrite_program, str_to_program
 
 class QueryGraph(object):
 
@@ -177,6 +177,8 @@ class QueryGraph(object):
             (Base case, one scene graph: True*; p11 ^ ... ^ p1i ^ d1; True*)
         Verbose form:
             q = Seq(Seq(Seq(Seq(True*, Duration(Conj(Conj(p13, p12), p11), theta1)), True*), Duration(Conj(Conj(p23, p22), p21), theta2)), True*)
+
+        Output: a list of all possible children of the current query graph. Each child is rewriten into the ordered format to avoid duplicates.
         """
         all_children = []
         # Action a: Scene graph construction: add a predicate to existing scene graph (i.e., the last scene graph in the sequence).
@@ -255,3 +257,129 @@ class QueryGraph(object):
             print("Action C: ", print_program(new_query_graph.program))
 
         return all_children
+
+    def get_all_children_unrestricted(self, predicate_dict, max_duration):
+        """
+        Example query:
+            q = True*; p11 ^ ... ^ p1i ^ d1; True*; p21 ^ ... ^ p2j ^ d2; True*
+            (Base case, one scene graph: True*; p11 ^ ... ^ p1i ^ d1; True*)
+        Verbose form:
+            q = Seq(Seq(Seq(Seq(True*, Duration(Conj(Conj(p13, p12), p11), theta1)), True*), Duration(Conj(Conj(p23, p22), p21), theta2)), True*)
+
+        Output: a list of all possible children of the current query graph. Each child is rewriten into the ordered format to avoid duplicates.
+        """
+        all_children = []
+        # Action a: Scene graph construction: add a predicate to existing scene graph (i.e., the last scene graph in the sequence).
+        # Require: the last scene graph must not have duration constraint.
+        if self.npred + 1 <= self.max_npred:
+            current_program = self.program
+            while True:
+                for pred in predicate_dict:
+                    pred_instances = []
+                    if predicate_dict[pred]:
+                        for param in predicate_dict[pred]:
+                            pred_instances.append(pred(param))
+                    else:
+                        pred_instances.append(pred())
+
+                    for pred_instance in pred_instances:
+                        # 1. Find the last scene graph g2 = q.submodules["function1"].submodules["function2"] // Duration(Conj(Conj(p23, p22), p21), theta2)
+                        parent_graph = [current_program.submodules["function1"], "function2"]
+                        last_graph = current_program.submodules["function1"].submodules["function2"]
+                        predicate_before_last_graph = current_program.submodules["function1"].submodules["function1"]
+                        # 2. If g2 has duration constraint, CONTINUE
+                        # NOT TRUE: locate the scene graph only: n2 = g2.submodules["duration"] // n2 = Conj(Conj(p23, p22), p21)
+                        if last_graph.name == "Duration":
+                            parent_graph = [last_graph, "duration"]
+                            last_graph = last_graph.submodules["duration"]
+                        # 3. Find p23, which is the leftmost child of n2. If the predicate p24 already exists in the scene graph, skip.
+                        is_duplicate_predicate = False
+                        while last_graph.name == "Conjunction":
+                            if last_graph.submodules["function2"] == pred_instance:
+                                is_duplicate_predicate = True
+                                break
+                            # print("test", parent_graph)
+                            parent_graph = [parent_graph[0].submodules[parent_graph[1]], "function1"]
+                            last_graph = last_graph.submodules["function1"] # Leftmost child of last_graph
+                        if is_duplicate_predicate:
+                            continue
+                        if last_graph == pred_instance:
+                            continue
+                        # 4. Replace p23 with Conj(p24, p23)
+                        orig_fclass = copy.deepcopy(last_graph)
+                        parent_graph[0].submodules[parent_graph[1]] = dsl.ConjunctionOperator(pred_instance, last_graph)
+                        # last_graph = dsl.ConjunctionOperator(pred_instance, last_graph)
+                        new_query_graph = copy.deepcopy(self)
+                        new_query_graph.npred += 1
+                        print("Action A: ", print_program(new_query_graph.program))
+                        all_children.append(new_query_graph)
+                        parent_graph[0].submodules[parent_graph[1]] = orig_fclass
+
+                if predicate_before_last_graph.name == "True*":
+                    break
+                else:
+                    current_program = predicate_before_last_graph
+
+        # Action b: Sequence construction: add a new scene graph (which consists of one predicate) to the end of the sequence.
+        # 1. q' = Seq(Seq(q, p31), True*)
+        if self.npred + 1 <= self.max_npred and self.depth + 1 <= self.max_depth:
+            for pred in predicate_dict:
+                pred_instances = []
+                if predicate_dict[pred]:
+                    for param in predicate_dict[pred]:
+                        pred_instances.append(pred(param))
+                else:
+                    pred_instances.append(pred())
+
+                for pred_instance in pred_instances:
+                    new_query_graph = copy.deepcopy(self)
+                    new_query_graph.program = dsl.SequencingOperator(dsl.SequencingOperator(new_query_graph.program, pred_instance), dsl.TrueStar())
+                    new_query_graph.npred += 1
+                    new_query_graph.depth += 1
+                    print("Action B: ", print_program(new_query_graph.program))
+                    all_children.append(new_query_graph)
+
+        # Action c: Duration refinement: increase the duration of the last scene graph in the sequence
+        current_program = self.program
+        while True:
+            # 1. Find the last scene graph g2 = q.submodules["function1"].submodules["function2"] // g2 = Duration(Conj(Conj(p23, p22), p21), theta2)
+            last_graph = current_program.submodules["function1"].submodules["function2"]
+            predicate_before_last_graph = current_program.submodules["function1"].submodules["function1"]
+            # 2. If g2 has duration constraint, increment by 1: g2.theta += 1
+            if last_graph.name == "Duration":
+                if last_graph.theta < max_duration:
+                    last_graph.theta += 1
+                    new_query_graph = copy.deepcopy(self)
+                    all_children.append(new_query_graph)
+                    print("Action C: ", print_program(new_query_graph.program))
+                    last_graph.theta -= 1
+            # 3. Else, add a duration constraint: g2' = Duration(g2, 2)
+            else:
+                orig_fclass = copy.deepcopy(last_graph)
+                current_program.submodules["function1"].submodules["function2"] = dsl.DurationOperator(last_graph, 2)
+                # last_graph = dsl.DurationOperator(last_graph, 2)
+                new_query_graph = copy.deepcopy(self)
+                all_children.append(new_query_graph)
+                print("Action C: ", print_program(new_query_graph.program))
+                current_program.submodules["function1"].submodules["function2"] = orig_fclass
+
+            if predicate_before_last_graph.name == "True*":
+                break
+            else:
+                current_program = predicate_before_last_graph
+
+        all_children_removing_duplicates = []
+        print("before removing duplicates:", len(all_children))
+        for child in all_children:
+            print(print_program(child.program))
+        signatures = set()
+        for child in all_children:
+            signature = rewrite_program(child.program)
+            if signature not in signatures:
+                child.program = str_to_program(signature)
+                all_children_removing_duplicates.append(child)
+                signatures.add(signature)
+        print("after removing duplicates:", len(all_children_removing_duplicates))
+        for child in all_children_removing_duplicates:
+            print(print_program(child.program))
+        return all_children_removing_duplicates
