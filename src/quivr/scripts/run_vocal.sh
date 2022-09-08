5bd2dbcfa4ebed5ef90a05e486739f1bb7644a65
Enhao Zhang <enhaoz@cs.washington.edu>
2022-09-08 16:24:12 -0700

    change

--- src/quivr/scripts/run_vocal.sh
+++ src/quivr/scripts/run_vocal.sh
@@ -9,4 +9,6 @@
 
 # Query 1: "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Front), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)"
 # Query 2: "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Conjunction(Front, Left)), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)"
-python synthesize.py --method vocal --npred 5 --depth 3 --k 32 --max_duration 2 --multithread 4 --query_str "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Conjunction(Front, Left)), True*), Duration(Left, 2)), True*), Conjunction(Far_0.9, Left)), True*)"
+# python synthesize.py --method vocal --n_init_pos 10 --n_init_neg 10 --npred 5 --depth 3 --max_duration 5 --beam_width 32 --k 100 --samples_per_iter 5 --multithread 4 --query_str "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Back), True*), Left), True*), Conjunction(Conjunction(Back, Left), Far_0.9)), True*)" --run_id 0
+
+python synthesize.py --method vocal --n_init_pos 10 --n_init_neg 10 --dataset_name "synthetic_rare" --npred 5 --depth 3 --max_duration 5 --beam_width 5 --k 100 --samples_per_iter 5 --multithread 4 --strategy "topk" --query_str "Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, BackOf), True*), Duration(Conjunction(Near_1.05, LeftOf), 4)), True*), Conjunction(TopQuadrant, Near_1.05)), True*)" --run_id 0 --output_to_file
