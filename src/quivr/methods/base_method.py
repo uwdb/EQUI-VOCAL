d6f207aa831fcb57aa00a89899bf617e70b40a0f
Enhao Zhang <enhaoz@cs.washington.edu>
2022-09-13 00:42:07 -0700

    add the postgres execution version of vocal

--- src/quivr/methods/base_method.py
+++ src/quivr/methods/base_method.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 from scipy import stats
 from concurrent.futures import ThreadPoolExecutor
-from quivr.utils import print_program, rewrite_program, str_to_program
+from quivr.utils import print_program, rewrite_program, str_to_program, postgres_execute
 import itertools
 
 def using(point=""):
@@ -27,7 +27,7 @@ class BaseMethod:
             memoize = self.memoize_all_inputs[i]
             if self.lock:
                 self.lock.release()
-            result, new_memoize  = current_query.execute(input, label, memoize, {})
+            result, new_memoize = current_query.execute(input, label, memoize, {})
             y_pred.append(int(result[0, len(input[0])] > 0))
             if self.lock:
                 self.lock.acquire()
@@ -40,77 +40,88 @@ class BaseMethod:
         score = f1_score(list(self.labels[self.labeled_index]), y_pred)
         return score
 
-    def pick_next_segment(self):
-        """
-        Pick the next segment to be labeled, based on disagreement among candidate queries (not their weights yet).
-        """
-        tuning_par = 1
-        true_labels = np.array(self.labels)
-        prediction_matrix = []
-        _start = time.time()
-        for i in range(len(self.inputs)):
-            # print("prediction matrix", i)
-            input = self.inputs[i]
-            label = self.labels[i]
-            if self.lock:
-                self.lock.acquire()
-            memoize = self.memoize_all_inputs[i]
-            if self.lock:
-                self.lock.release()
-            pred_per_input = []
-            for query_graph, _ in self.candidate_queries:
-                query = query_graph.program
-                result, new_memoize = query.execute(input, label, memoize, {})
-                pred_per_input.append(int(result[0, len(input[0])] > 0))
-                if self.lock:
-                    self.lock.acquire()
-                self.memoize_all_inputs[i].update(new_memoize)
-                if self.lock:
-                    self.lock.release()
-            prediction_matrix.append(pred_per_input)
-        prediction_matrix = np.array(prediction_matrix)
-        print("constructing prediction matrix", time.time()-_start)
-        n_instances = len(true_labels)
-
-        # # Initialize
-        # loss_t = np.zeros(self.beam_width)
-        # for i in range(n_instances):
-        #     if i not in self.labeled_index:
-        #         continue
-        #     if true_labels[i]:
-        #         loss_t += (np.array((prediction_matrix[i, :] != 1) * 1) * 5)
-        #         # loss_t += (np.array((prediction_matrix_pair_level[t, :] != 1) * 1) / u_t * (n_pos + n_neg) / (2 * n_pos))
-        #         # n_pos += 1
-        #     else:
-        #         loss_t += (np.array((prediction_matrix[i, :] != 0) * 1))
-
-        # eta = np.sqrt(np.log(self.beam_width)/(2*(len(self.labeled_index)+1)))
-
-        # posterior_t = np.exp(-eta * (loss_t-np.min(loss_t)))
-        # # Note that above equation is equivalent to np.exp(-eta * loss_t).
-        # # `-np.min(loss_t)` is applied only to avoid entries being near zero for large eta*loss_t values before the normalization
-        # posterior_t  /= np.sum(posterior_t)  # normalized weight
-
-        entropy_list = np.zeros(n_instances)
-        for i in range(n_instances):
-            if i in self.labeled_index:
-                entropy_list[i] = -1
+    def compute_query_score_postgres(self, current_query):
+        vids = postgres_execute(current_query, self.labeled_index)
+        y_pred = []
+        for i in self.labeled_index:
+            if i in vids:
+                y_pred.append(1)
             else:
-                # Measure the normalized entropy of the incoming data
-                hist, bin_edges = np.histogram(prediction_matrix[i, :], bins=2)
-                prob_i = hist/np.sum(hist)
-                entropy_i = stats.entropy(prob_i, base=2) * tuning_par
-                # Check if the normalized entropy is greater than 1
-                if entropy_i > 1:
-                    entropy_i = 1
-                if entropy_i < 0:
-                    entropy_i = 0
-                entropy_list[i] = entropy_i
-        # find argmax of entropy (top k)
-        n_to_label = 1
-        video_segment_ids = np.argpartition(entropy_list, -n_to_label)[-n_to_label:]
-        # max_entropy_index = np.argmax(entropy_list)
-        return video_segment_ids.tolist()
+                y_pred.append(0)
+        score = f1_score(list(self.labels[self.labeled_index]), y_pred)
+        return score
+
+    # def pick_next_segment(self):
+    #     """
+    #     Pick the next segment to be labeled, based on disagreement among candidate queries (not their weights yet).
+    #     """
+    #     tuning_par = 1
+    #     true_labels = np.array(self.labels)
+    #     prediction_matrix = []
+    #     _start = time.time()
+    #     for i in range(len(self.inputs)):
+    #         # print("prediction matrix", i)
+    #         input = self.inputs[i]
+    #         label = self.labels[i]
+    #         if self.lock:
+    #             self.lock.acquire()
+    #         memoize = self.memoize_all_inputs[i]
+    #         if self.lock:
+    #             self.lock.release()
+    #         pred_per_input = []
+    #         for query_graph, _ in self.candidate_queries:
+    #             query = query_graph.program
+    #             result, new_memoize = query.execute(input, label, memoize, {})
+    #             pred_per_input.append(int(result[0, len(input[0])] > 0))
+    #             if self.lock:
+    #                 self.lock.acquire()
+    #             self.memoize_all_inputs[i].update(new_memoize)
+    #             if self.lock:
+    #                 self.lock.release()
+    #         prediction_matrix.append(pred_per_input)
+    #     prediction_matrix = np.array(prediction_matrix)
+    #     print("constructing prediction matrix", time.time()-_start)
+    #     n_instances = len(true_labels)
+
+    #     # # Initialize
+    #     # loss_t = np.zeros(self.beam_width)
+    #     # for i in range(n_instances):
+    #     #     if i not in self.labeled_index:
+    #     #         continue
+    #     #     if true_labels[i]:
+    #     #         loss_t += (np.array((prediction_matrix[i, :] != 1) * 1) * 5)
+    #     #         # loss_t += (np.array((prediction_matrix_pair_level[t, :] != 1) * 1) / u_t * (n_pos + n_neg) / (2 * n_pos))
+    #     #         # n_pos += 1
+    #     #     else:
+    #     #         loss_t += (np.array((prediction_matrix[i, :] != 0) * 1))
+
+    #     # eta = np.sqrt(np.log(self.beam_width)/(2*(len(self.labeled_index)+1)))
+
+    #     # posterior_t = np.exp(-eta * (loss_t-np.min(loss_t)))
+    #     # # Note that above equation is equivalent to np.exp(-eta * loss_t).
+    #     # # `-np.min(loss_t)` is applied only to avoid entries being near zero for large eta*loss_t values before the normalization
+    #     # posterior_t  /= np.sum(posterior_t)  # normalized weight
+
+    #     entropy_list = np.zeros(n_instances)
+    #     for i in range(n_instances):
+    #         if i in self.labeled_index:
+    #             entropy_list[i] = -1
+    #         else:
+    #             # Measure the normalized entropy of the incoming data
+    #             hist, bin_edges = np.histogram(prediction_matrix[i, :], bins=2)
+    #             prob_i = hist/np.sum(hist)
+    #             entropy_i = stats.entropy(prob_i, base=2) * tuning_par
+    #             # Check if the normalized entropy is greater than 1
+    #             if entropy_i > 1:
+    #                 entropy_i = 1
+    #             if entropy_i < 0:
+    #                 entropy_i = 0
+    #             entropy_list[i] = entropy_i
+    #     # find argmax of entropy (top k)
+    #     n_to_label = 1
+    #     video_segment_ids = np.argpartition(entropy_list, -n_to_label)[-n_to_label:]
+    #     # max_entropy_index = np.argmax(entropy_list)
+    #     return video_segment_ids.tolist()
 
     def execute_over_all_inputs(self, query):
         pred_per_query = []
