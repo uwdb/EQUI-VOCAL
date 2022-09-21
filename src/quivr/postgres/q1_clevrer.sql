8279a5fe4469796027f45d1aeef13cdda5c99902
Enhao Zhang <enhaoz@cs.washington.edu>
2022-09-20 17:13:31 -0700

    update example query execution statements

--- src/quivr/postgres/q1_clevrer.sql
+++ src/quivr/postgres/q1_clevrer.sql
@@ -1,37 +1,37 @@
 /*
-Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Near_1.05), True*), Conjunction(LeftOf, BackOf)), True*), Duration(Conjunction(TopQuadrant, Far_0.9), 5)), True*)
+Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Near_1.05), True*), Conjunction(LeftOf, Behind)), True*), Duration(Conjunction(TopQuadrant, Far_0.9), 5)), True*)
 */
 \timing
 
-DROP FUNCTION IF EXISTS Near, Far, LeftOf, BackOf, RightQuadrant, TopQuadrant;
-DROP INDEX IF EXISTS idx_obj_filtered, idx_g1, idx_g2, idx_g3, idx_g1_seq_view, idx_g2_seq_view, idx_g3_seq_view, idx_q1;
+-- DROP FUNCTION IF EXISTS Near, Far, LeftOf, BackOf, RightQuadrant, TopQuadrant;
+-- DROP INDEX IF EXISTS idx_obj_filtered, idx_g1, idx_g2, idx_g3, idx_g1_seq_view, idx_g2_seq_view, idx_g3_seq_view, idx_q1;
 
-CREATE FUNCTION Near(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'near'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION Near(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'near'
+--     LANGUAGE C STRICT;
 
-CREATE FUNCTION Far(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'far'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION Far(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'far'
+--     LANGUAGE C STRICT;
 
-CREATE FUNCTION LeftOf(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'left_of'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION LeftOf(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'left_of'
+--     LANGUAGE C STRICT;
 
-CREATE FUNCTION BackOf(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'behind'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION BackOf(double precision, double precision, double precision, double precision, double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'behind'
+--     LANGUAGE C STRICT;
 
-CREATE FUNCTION RightQuadrant(double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'right_quadrant'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION RightQuadrant(double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'right_quadrant'
+--     LANGUAGE C STRICT;
 
-CREATE FUNCTION TopQuadrant(double precision, double precision, double precision, double precision) RETURNS boolean
-    AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'top_quadrant'
-    LANGUAGE C STRICT;
+-- CREATE FUNCTION TopQuadrant(double precision, double precision, double precision, double precision) RETURNS boolean
+--     AS '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/functors', 'top_quadrant'
+--     LANGUAGE C STRICT;
 
 CREATE TEMPORARY TABLE Obj_filtered AS
-SELECT * FROM Obj_trajectories WHERE vid in generate_series(0, 299);
+SELECT * FROM Obj_trajectories;
 
 CREATE INDEX idx_obj_filtered
 ON Obj_filtered (vid, fid);
@@ -39,31 +39,11 @@ ON Obj_filtered (vid, fid);
 CREATE TEMPORARY TABLE g1 AS
 SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
 FROM Obj_filtered a, Obj_filtered b
-WHERE a.vid = b.vid and a.fid = b.fid and near(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
-
-CREATE TEMPORARY TABLE g2 AS
-SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
-FROM Obj_filtered a, Obj_filtered b
-WHERE a.vid = b.vid and a.fid = b.fid and left_of(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and behind(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
-
-CREATE TEMPORARY TABLE g3 AS
-SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
-FROM Obj_filtered a, Obj_filtered b
-WHERE a.vid = b.vid and a.fid = b.fid and top_quadrant(a.x1, a.y1, a.x2, a.y2) = true and far(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
+WHERE a.vid = b.vid and a.fid = b.fid and Near(1.05, a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
 
 CREATE INDEX idx_g1
 ON g1 (vid, oid1, oid2, fid);
 
-CREATE INDEX idx_g2
-ON g2 (vid, oid1, oid2, fid);
-
-CREATE INDEX idx_g3
-ON g3 (vid, oid1, oid2, fid);
-
--- EXPLAIN ANALYZE
-
--- BEGIN;
--- SET LOCAL enable_mergejoin TO false;
 CREATE TEMPORARY TABLE g1_seq_view AS
 WITH RECURSIVE g1_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- base case
@@ -72,9 +52,20 @@ WITH RECURSIVE g1_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- step case
     SELECT s.vid, s.fid1, g.fid, s.oid1, s.oid2
     FROM g1_seq s, g1 g
-    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1
+    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1 AND g.fid - s.fid1 + 1 <= 1
 )
-SELECT DISTINCT vid, fid1, fid2, oid1, oid2 FROM g1_seq;
+SELECT DISTINCT * FROM g1_seq WHERE fid2 - fid1 + 1 = 1;
+
+CREATE INDEX idx_g1_seq_view
+ON g1_seq_view (vid, fid1, fid2, oid1, oid2);
+
+CREATE TEMPORARY TABLE g2 AS
+SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
+FROM Obj_filtered a, Obj_filtered b
+WHERE a.vid = b.vid and a.fid = b.fid and LeftOf(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and Behind(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
+
+CREATE INDEX idx_g2
+ON g2 (vid, oid1, oid2, fid);
 
 CREATE TEMPORARY TABLE g2_seq_view AS
 WITH RECURSIVE g2_seq (vid, fid1, fid2, oid1, oid2) AS (
@@ -84,9 +75,20 @@ WITH RECURSIVE g2_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- step case
     SELECT s.vid, s.fid1, g.fid, s.oid1, s.oid2
     FROM g2_seq s, g2 g
-    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1
+    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1 AND g.fid - s.fid1 + 1 <= 1
 )
-SELECT DISTINCT vid, fid1, fid2, oid1, oid2 FROM g2_seq;
+SELECT DISTINCT * FROM g2_seq WHERE fid2 - fid1 + 1 = 1;
+
+CREATE INDEX idx_g2_seq_view
+ON g2_seq_view (vid, fid1, fid2, oid1, oid2);
+
+CREATE TEMPORARY TABLE g3 AS
+SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
+FROM Obj_filtered a, Obj_filtered b
+WHERE a.vid = b.vid and a.fid = b.fid and TopQuadrant(a.x1, a.y1, a.x2, a.y2) = true and Far(0.9, a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
+
+CREATE INDEX idx_g3
+ON g3 (vid, oid1, oid2, fid);
 
 CREATE TEMPORARY TABLE g3_seq_view AS
 WITH RECURSIVE g3_seq (vid, fid1, fid2, oid1, oid2) AS (
@@ -96,34 +98,26 @@ WITH RECURSIVE g3_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- step case
     SELECT s.vid, s.fid1, g.fid, s.oid1, s.oid2
     FROM g3_seq s, g3 g
-    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1
+    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1 AND g.fid - s.fid1 + 1 <= 5
 )
-SELECT DISTINCT vid, fid1, fid2, oid1, oid2 FROM g3_seq WHERE fid2 - fid1 + 1 >= 5;
-
-
-CREATE INDEX idx_g1_seq_view
-ON g1_seq_view (vid, fid1, fid2, oid1, oid2);
-
-CREATE INDEX idx_g2_seq_view
-ON g2_seq_view (vid, fid1, fid2, oid1, oid2);
+SELECT DISTINCT * FROM g3_seq WHERE fid2 - fid1 + 1 = 5;
 
 CREATE INDEX idx_g3_seq_view
 ON g3_seq_view (vid, fid1, fid2, oid1, oid2);
 
 CREATE TEMPORARY TABLE q1 AS
-SELECT DISTINCT g1.vid as vid, g1.fid1 as fid1, g2.fid2 as fid2, g1.oid1 as oid1, g1.oid2 as oid2
+SELECT g1.vid as vid, min(g2.fid2) as fid, g1.oid1 as oid1, g1.oid2 as oid2
 FROM g1_seq_view g1, g2_seq_view g2
-WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid2 < g2.fid1;
+WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid2 < g2.fid1
+GROUP BY g1.vid, g1.oid1, g1.oid2;
 
 CREATE INDEX idx_q1
-ON q1 (vid, fid1, fid2, oid1, oid2);
+ON q1 (vid, fid, oid1, oid2);
 
-CREATE TEMPORARY TABLE q AS
-SELECT DISTINCT g1.vid as vid, g1.fid1 as fid1, g2.fid2 as fid2, g1.oid1 as oid1, g1.oid2 as oid2
-FROM q1 as g1, g3_seq_view g2
-WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid2 < g2.fid1;
+CREATE TEMPORARY TABLE q2 AS
+SELECT g1.vid as vid, min(g2.fid2) as fid, g1.oid1 as oid1, g1.oid2 as oid2
+FROM q1 g1, g3_seq_view g2
+WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid < g2.fid1
+GROUP BY g1.vid, g1.oid1, g1.oid2;
 
--- SELECT distinct vid from q;
-
--- \copy (SELECT * FROM q) TO '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/outputs/q1_clevrer.csv' (format csv);
--- select * from q;
+SELECT count(distinct vid) from q2;
--- src/quivr/postgres/q2_clevrer.sql
+++ src/quivr/postgres/q2_clevrer.sql
@@ -1,7 +1,7 @@
 /*
 Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Near_1.05), True*), Conjunction(LeftOf, BackOf)), True*), Duration(Conjunction(TopQuadrant, Far_0.9), 5)), True*)
 */
-\timing
+-- \timing
 
 -- DROP FUNCTION IF EXISTS Near, Far, LeftOf, BackOf, RightQuadrant, TopQuadrant;
 
@@ -30,31 +30,20 @@ Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(Sequencing(True*, Near_1.
 --     LANGUAGE C STRICT;
 
 CREATE TEMPORARY TABLE Obj_filtered AS
-SELECT * FROM Obj_trajectories WHERE vid = 1;
+SELECT * FROM Obj_trajectories;
 
 CREATE INDEX idx_obj_filtered
 ON Obj_filtered (vid, fid);
 
+-- g1
 CREATE TEMPORARY TABLE g1 AS
 SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
 FROM Obj_filtered a, Obj_filtered b
 WHERE a.vid = b.vid and a.fid = b.fid and LeftOf(a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
 
-CREATE TEMPORARY TABLE g2 AS
-SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
-FROM Obj_filtered a, Obj_filtered b
-WHERE a.vid = b.vid and a.fid = b.fid and Near(1.05, a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
-
 CREATE INDEX idx_g1
 ON g1 (vid, oid1, oid2, fid);
 
-CREATE INDEX idx_g2
-ON g2 (vid, oid1, oid2, fid);
-
--- EXPLAIN ANALYZE
-
--- BEGIN;
--- SET LOCAL enable_mergejoin TO false;
 CREATE TEMPORARY TABLE g1_seq_view AS
 WITH RECURSIVE g1_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- base case
@@ -63,17 +52,24 @@ WITH RECURSIVE g1_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- step case
     SELECT s.vid, s.fid1, g.fid, s.oid1, s.oid2
     FROM g1_seq s, g1 g
-    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1
+    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1 AND g.fid - s.fid1 + 1 <= 1
 )
-SELECT vid, max(fid1) AS fid1, fid2, oid1, oid2
-FROM (
-    SELECT vid, fid1, min(fid2) AS fid2, oid1, oid2 FROM g1_seq
-    WHERE fid2 - fid1 + 1 >= duration_constraint
-    GROUP BY vid, fid1, oid1, oid2
-) AS t
-GROUP BY vid, fid2, oid1, oid2;
+SELECT DISTINCT * FROM g1_seq WHERE fid2 - fid1 + 1 = 1;
 
 SELECT COUNT(*) FROM g1_seq_view;
+
+CREATE INDEX idx_g1_seq_view
+ON g1_seq_view (vid, oid1, oid2, fid1, fid2);
+
+
+-- g2
+CREATE TEMPORARY TABLE g2 AS
+SELECT a.oid as oid1, b.oid as oid2, a.vid, a.fid
+FROM Obj_filtered a, Obj_filtered b
+WHERE a.vid = b.vid and a.fid = b.fid and Near(1.05, a.x1, a.y1, a.x2, a.y2, b.x1, b.y1, b.x2, b.y2) = true and a.oid = 0 and b.oid = 1;
+
+CREATE INDEX idx_g2
+ON g2 (vid, oid1, oid2, fid);
 
 CREATE TEMPORARY TABLE g2_seq_view AS
 WITH RECURSIVE g2_seq (vid, fid1, fid2, oid1, oid2) AS (
@@ -83,28 +79,22 @@ WITH RECURSIVE g2_seq (vid, fid1, fid2, oid1, oid2) AS (
     -- step case
     SELECT s.vid, s.fid1, g.fid, s.oid1, s.oid2
     FROM g2_seq s, g2 g
-    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1
+    WHERE s.vid = g.vid AND s.oid1 = g.oid1 AND s.oid2 = g.oid2 AND g.fid = s.fid2 + 1 AND g.fid - s.fid1 + 1 <= 1
 )
-SELECT vid, max(fid1) AS fid1, fid2, oid1, oid2
-FROM (
-    SELECT vid, fid1, min(fid2) AS fid2, oid1, oid2 FROM g2_seq
-    WHERE fid2 - fid1 + 1 >= 1
-    GROUP BY vid, fid1, oid1, oid2
-) AS t
-GROUP BY vid, fid2, oid1, oid2;
+SELECT DISTINCT * FROM g2_seq WHERE fid2 - fid1 + 1 = 1;
 
 SELECT COUNT(*) FROM g2_seq_view;
-
-CREATE INDEX idx_g1_seq_view
-ON g1_seq_view (vid, oid1, oid2, fid1, fid2);
 
 CREATE INDEX idx_g2_seq_view
 ON g2_seq_view (vid, oid1, oid2, fid1, fid2);
 
+
+-- g1, g2
 CREATE TEMPORARY TABLE q1 AS
-SELECT DISTINCT g1.vid as vid, g1.fid1 as fid1, g2.fid2 as fid2, g1.oid1 as oid1, g1.oid2 as oid2
+SELECT g1.vid as vid, min(g2.fid2) as fid, g1.oid1 as oid1, g1.oid2 as oid2
 FROM g1_seq_view g1, g2_seq_view g2
-WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid2 < g2.fid1;
+WHERE g1.vid = g2.vid AND g1.oid1 = g2.oid1 AND g1.oid2 = g2.oid2 AND g1.fid2 < g2.fid1
+GROUP BY g1.vid, g1.oid1, g1.oid2;
 
 SELECT count(distinct vid) from q1;
 
