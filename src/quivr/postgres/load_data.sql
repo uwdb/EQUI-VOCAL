bcc2e94359346d751539cd1b0c717e139683af7f
Enhao Zhang <enhaoz@cs.washington.edu>
2022-09-20 17:10:55 -0700

    create index for obj table

--- src/quivr/postgres/load_data.sql
+++ src/quivr/postgres/load_data.sql
@@ -1,2 +1,3 @@
-COPY Obj_clevrer FROM '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/obj_clevrer_test.csv' DELIMITER ',' CSV;
+COPY Obj_clevrer FROM '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/obj_clevrer.csv' DELIMITER ',' CSV;
+CREATE INDEX IF NOT EXISTS idx_obj_clevrer ON Obj_clevrer (vid);
 -- COPY Obj_trajectories FROM '/mmfs1/gscratch/balazinska/enhaoz/complex_event_video/src/quivr/postgres/test_trajectories.csv' DELIMITER ',' CSV;
